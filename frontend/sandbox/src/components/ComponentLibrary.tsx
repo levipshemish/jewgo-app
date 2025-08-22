ebdf02526df1a3f5c47f1a6f5fcea6ab625f2175
mml555 <mendel1023@gmail.com>
2025-08-22 02:15:39 -0400

    Fix import path for componentExamples

--- frontend/sandbox/src/components/ComponentLibrary.tsx
+++ frontend/sandbox/src/components/ComponentLibrary.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { MagnifyingGlassIcon, FunnelIcon, ViewColumnsIcon, ListBulletIcon } from '@heroicons/react/24/outline'
-import { componentExamples } from '../data/componentExamples.tsx'
+import { componentExamples } from '../data/componentExamples'
 
 interface ComponentLibraryProps {
   onComponentSelect: (componentName: string) => void
