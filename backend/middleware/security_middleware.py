2ee83cf97c13c9bf56c7ee67fe70f958c72baff6
mml555 <mendel1023@gmail.com>
2025-09-15 23:02:55 -0400

    fix: add X-CSRF-Token to security middleware CORS headers

--- backend/middleware/security_middleware.py
+++ backend/middleware/security_middleware.py
@@ -122,7 +122,7 @@ class SecurityMiddleware:
                 response.headers['Access-Control-Allow-Origin'] = origin
                 response.headers['Access-Control-Allow-Credentials'] = 'true'
                 response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-                response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+                response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With, X-CSRF-Token'
             
             # Remove server information
             response.headers.pop('Server', None)
